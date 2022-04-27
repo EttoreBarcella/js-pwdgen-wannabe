d27de782073b520fbc1cb821adf4b17bd5a5aa13
EttoreBarcella <ettore.barcella@gmail.com>
2022-04-27 13:10:34 +0200

    added prompt

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
-// Chiedi all’utente il suo nome,
-// poi chiedi il suo cognome,
+// Chiedi all’utente il suo nome
+const name = prompt("Inserisci il tuo nome");
+// poi chiedi il suo cognome
+const surname = prompt("Inserisci il tuo cognome");
 // poi chiedi il suo colore preferito
+const favColor = prompt("Inserisci il tuo colore preferito");
 // Infine scrivi sulla pagina nomecognomecolorepreferito21
